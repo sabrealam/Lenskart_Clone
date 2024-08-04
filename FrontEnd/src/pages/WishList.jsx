de52660158317daae26721df4d32eea913295ceb
Sabre Alam <142332803+sabrealam@users.noreply.github.com>
2024-08-04 17:52:52 +0530

    Testing

--- FrontEnd/src/pages/WishList.jsx
+++ FrontEnd/src/pages/WishList.jsx
@@ -1,37 +1,74 @@
-import React from 'react'
-import { Box, Button, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay, Grid, Radio, RadioGroup, Stack, useDisclosure } from '@chakra-ui/react'
-import axios from 'axios'
-import { useSelector } from 'react-redux'
-export default function WishList( { isOpen, onOpen, onClose} ) {
-    const [placement, setPlacement] = React.useState('left')
-    let state = useSelector(state => state.render)
-    let [data, setData] = React.useState([1,2,3,4,5])
-
-    React.useEffect(()=>{
-        setData([1,2,3,4,5])
-    },[])
+import React from "react";
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Grid,
+  Radio,
+  RadioGroup,
+  Stack,
+  useDisclosure,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { GET_DATA } from "../redux/actionTypes";
+import ItemBox from "../components/ItemBox";
+export default function WishList({ isOpen, onOpen, onClose }) {
+  const [placement, setPlacement] = React.useState("left");
+  let render = useSelector((state) => state.render);
+  let dispatch = useDispatch();
+  let [data, setData] = React.useState([1, 2, 3, 4, 5]);
+  let user = JSON.parse(localStorage.getItem("user"));
+  let url = import.meta.env.VITE_BASE_URL;
+  console.log(render);
+  React.useEffect(() => {
+    let viewFav = async () => {
+      try {
+        let { data } = await axios({
+          method: "post",
+          url: `${url}/viewfavorite`,
+          data: { email: user.user.email },
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+            "Content-Type": "application/json",
+          },
+        });
+        console.log(data);
+        dispatch({ type: GET_DATA, payload: data });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    viewFav();
+  }, []);
   return (
     <>
-      <RadioGroup defaultValue={placement} onChange={setPlacement}>
-        
-      </RadioGroup>
- 
-      <Drawer placement={placement} onClose={onClose} isOpen={isOpen} size={"xl"}>
+      <RadioGroup defaultValue={placement} onChange={setPlacement}></RadioGroup>
+
+      <Drawer
+        placement={placement}
+        onClose={onClose}
+        isOpen={isOpen}
+        size={"xl"}
+      >
         <DrawerOverlay />
         <DrawerContent>
-          <DrawerHeader borderBottomWidth='1px'>Wishlist</DrawerHeader>
+          <DrawerHeader borderBottomWidth="1px">Wishlist</DrawerHeader>
           <DrawerBody>
-            <Grid templateColumns={"repeat(5,1fr)"} gap={"5px"}>
-                {data.map((data, item)=>{
-                    return(
-                        <Box key={item} w={"100px"} h={"100px"} border={"1px solid black"} >fav</Box>
-                    )
-                })}
+            <Grid templateColumns={"repeat(3,1fr)"} gap={"5px"}>
+              {render.map((data, item) => {
+                return (
+                  <ItemBox key={item.id} {...data} />
+                );
+              })}
             </Grid>
-            
           </DrawerBody>
         </DrawerContent>
       </Drawer>
     </>
-  )
+  );
 }
